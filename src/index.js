63798b884778753027d0612c9cb2558858b136b8
vikas <vk98659@gmail.com>
2026-05-05 13:43:47 +0530

    check 1

--- src/index.js
+++ src/index.js
@@ -295,12 +295,39 @@ const BASE_URL = process.env.PHONEPE_BASE_URL;
 const SERVER_URL = process.env.DOMAIN_URL;
 
 // FIREBASE INIT
+// admin.initializeApp({
+//   credential: admin.credential.cert({
+//     type: process.env.SERVICE_ACCOUNT_TYPE,
+//     project_id: process.env.SERVICE_PROJECT_ID,
+//     private_key_id: process.env.SERVICE_PRIVATE_KEY_ID,
+//     private_key: process.env.SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//     client_email: process.env.SERVICE_CLIENT_EMAIL,
+//     client_id: process.env.SERVICE_CLIENT_ID,
+//     auth_uri: process.env.SERVICE_AUTH_URL,
+//     token_uri: process.env.SERVICE_TOKEN_URI,
+//     auth_provider_x509_cert_url: process.env.SERVICE_AUTH_PROVIDER_X509_CERT_URL,
+//     client_x509_cert_url: process.env.SERVICE_CLIENT_X509_CERT_URL,
+//     universe_domain: process.env.SERVICE_UNIVERSE_DOMAIN,
+//   }),
+// });
+console.log(" Starting Firebase Admin Init...");
+
+console.log(" ENV CHECK:");
+console.log("PROJECT_ID:", process.env.SERVICE_PROJECT_ID);
+console.log("CLIENT_EMAIL:", process.env.SERVICE_CLIENT_EMAIL);
+console.log(
+  "PRIVATE_KEY:",
+  process.env.SERVICE_PRIVATE_KEY ? "EXISTS" : "MISSING"
+);
+
 admin.initializeApp({
   credential: admin.credential.cert({
     type: process.env.SERVICE_ACCOUNT_TYPE,
     project_id: process.env.SERVICE_PROJECT_ID,
     private_key_id: process.env.SERVICE_PRIVATE_KEY_ID,
-    private_key: process.env.SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.SERVICE_PRIVATE_KEY
+      ? process.env.SERVICE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      : undefined,
     client_email: process.env.SERVICE_CLIENT_EMAIL,
     client_id: process.env.SERVICE_CLIENT_ID,
     auth_uri: process.env.SERVICE_AUTH_URL,
@@ -310,6 +337,22 @@ admin.initializeApp({
     universe_domain: process.env.SERVICE_UNIVERSE_DOMAIN,
   }),
 });
+
+console.log("🔥 Firebase Admin Initialized", {
+    type: process.env.SERVICE_ACCOUNT_TYPE,
+    project_id: process.env.SERVICE_PROJECT_ID,
+    private_key_id: process.env.SERVICE_PRIVATE_KEY_ID,
+    private_key: process.env.SERVICE_PRIVATE_KEY
+      ? process.env.SERVICE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      : undefined,
+    client_email: process.env.SERVICE_CLIENT_EMAIL,
+    client_id: process.env.SERVICE_CLIENT_ID,
+    auth_uri: process.env.SERVICE_AUTH_URL,
+    token_uri: process.env.SERVICE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.SERVICE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.SERVICE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.SERVICE_UNIVERSE_DOMAIN,
+  });
 const db = admin.firestore();
 
 // =======================================
